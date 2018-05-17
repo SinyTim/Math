203f97538cee351baf42a04f22020de35d202843
Timur <SinyTim@mail.ru>
2018-05-17 18:06:47 +0300

    .

---
+++ Matrix.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+
+namespace Math {
+
+
+template <class Type>
+class Matrix {
+
+public:
+    Matrix(); 
+    Matrix(int rows, int columns);
+    Matrix(const Matrix& obj);
+    ~Matrix();
+    Type& operator() (int row, int column);
+    Matrix<Type>& operator= (const Matrix& matrix);
+    Matrix<Type>& operator+= (const Matrix& matrix);
+    Matrix<Type>& operator-= (const Matrix& matrix);
+    Matrix<Type>& operator*= (const Type& scalar);
+    Matrix<Type>& operator*= (const Matrix& matrix);
+    Matrix<Type> operator+ (const Matrix& matrix) const;
+    Matrix<Type> operator- (const Matrix& matrix) const;
+    Matrix<Type> operator* (const Type& scalar) const;
+    Matrix<Type> operator* (const Matrix& matrix) const;
+    Matrix<Type> operator^ (unsigned int degree) const;
+    bool operator== (const Matrix& obj) const;
+    Type det() const;
+    int rank() const;
+    Matrix<Type> T() const;
+    Matrix<Type> invert() const;
+
+
+private:
+    const int rows;
+    const int columns;
+    Type** data;
+};
+
+
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Matrix.h"
+
+
+int main() {
+    //Math::Matrix<int> matrix;
+
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
