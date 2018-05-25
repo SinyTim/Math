5437c629797e1f2cc7aea0039dcd4c9af61986b4
Timur <SinyTim@mail.ru>
2018-05-25 20:26:37 +0300

    .

--- test.cpp
+++ test.cpp
@@ -6,7 +6,23 @@
 using namespace std;
 
 
+void testMatrix();
+void testComplex();
+
+
+
 int main() {
+    //testMatrix();
+    testComplex();
+
+    cout << "\n";
+    system("pause");
+    return 0;
+}
+
+
+
+void testMatrix() {
     math::MatrixSize size0(5, 6);
     int** arr = new int*[size0.rows()];
     for (size_t i = 0; i < size0.rows(); ++i) {
@@ -23,24 +39,28 @@ int main() {
     cout << "Matrix2: " << m2 << "\n";
     cout << "Matrix3: " << m3 << "\n";
 
-    math::ComplexNumber c1(2, -2), c2(2, -2);
+    math::Matrix<math::ComplexNumber> mc = {
+        {{2, 1}, {-1, 4}},
+        {{0, 0}, {3, -7}}
+    };
+    cout << "Complex matrix: " << mc;
+}
 
-    if(c1==c2)
-        cout << "Ok\n";
+void testComplex() {
+    math::ComplexNumber c1(2, -2), c2(2, -2);
+    c2 *= 1.0000000001;
+
+    if (c1 == c2) {
+        cout << c1 << " == " << c2;
+    } else {
+        cout << c1 << " != " << c2;
+    }
+    cout << '\n';
 
     math::ComplexNumber c3(5, 10), c4(3, 4);
     cout << c3 * c4 << endl;
     cout << c3 / c4 << endl;
 
-    c4 = c4.conjugate();
+    cout << c4.conjugate() << endl;
     cout << c4.modulus() << endl;
-
-    math::Matrix<math::ComplexNumber> mc = {
-        {{2, 1}, {-1, 4}}, 
-        {{0, 0}, c1}
-    };
-    cout << "Complex matrix: " << mc;
-
-    cout << "\n";
-    return 0;
 }
